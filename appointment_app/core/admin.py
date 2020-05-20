524524d3d1a59ca9306790d851fdcf4de2a0ef47
Alfonso Feria <alfonso.feria@konfio.mx>
2020-05-19 20:11:50 -0500

    Added Models to Appointment

--- appointment_app/core/admin.py
+++ appointment_app/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
+from .models import User, Pediatrician, AppointmentScheduling, Appointment
 from django.utils.translation import gettext as _
 
 
@@ -11,14 +11,16 @@ class UserAdmin(BaseUserAdmin):
     fieldsets = (
         (None, {"fields": ("email", "password")}),
         (_("Personal Info"), {"fields": ("name",)}),
-        (_("Permissions"), {
-         "fields": ("is_active", "is_staff", "is_superuser",)}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser",)}),
         (_("Important dates"), {"fields": ("last_login",)}),
     )
     add_fieldsets = (
-        (None, {"classes": ("wide",), "fields": (
-            "email", "password1", "password1",)}),
+        (None, {"classes": ("wide",), "fields": ("email", "password1", "password1",)}),
     )
 
 
+admin.site.site_header = "Yema Test Alfonso"
 admin.site.register(User, UserAdmin)
+admin.site.register(Pediatrician)
+admin.site.register(AppointmentScheduling)
+admin.site.register(Appointment)
--- appointment_app/core/models.py
+++ appointment_app/core/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -5,6 +6,14 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 from django.conf import settings
+
+
+def get_hour_appointment(date):
+    return timezone.localtime(date).strftime("%H:%M %p")
+
+
+def get_date_appointment(date):
+    return timezone.localtime(date).strftime("%Y-%m-%d")
 
 
 class UserManager(BaseUserManager):
@@ -40,3 +49,61 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
+
+
+GENRE = (
+    ("M", "Masculino"),
+    ("F", "Femenino"),
+)
+
+
+class AuditTrail(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Pediatrician(AuditTrail):
+    name = models.CharField(max_length=250, blank=True)
+    genre = models.CharField(max_length=1, choices=GENRE, default="M")
+
+    class Meta:
+        verbose_name = "Pediatra"
+        verbose_name_plural = "Pediatras"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.name
+
+
+class AppointmentScheduling(AuditTrail):
+    pediatrician = models.ForeignKey(Pediatrician, on_delete=models.CASCADE)
+    time_start = models.DateTimeField()
+    time_finish = models.DateTimeField()
+    is_available = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Programación de cita"
+        verbose_name_plural = "Programación de citas"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return f"{self.pediatrician.name} el {get_date_appointment(self.time_start)}  de {get_hour_appointment(self.time_start)} a {get_hour_appointment(self.time_finish)}"
+
+
+class Appointment(AuditTrail):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    appointment_scheduling = models.ForeignKey(
+        AppointmentScheduling, on_delete=models.CASCADE
+    )
+    comments = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name = "Cita"
+        verbose_name_plural = "Citas"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.user.email
