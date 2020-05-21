c367d534b8563a8b713b6e599a1f8a097f9c8581
Alfonso Feria <alfonso.feria@konfio.mx>
2020-05-21 17:27:51 -0500

    TDD Not Available Appointment

--- appointment_app/service/tests/test_appointment_api.py
+++ appointment_app/service/tests/test_appointment_api.py
@@ -40,6 +40,23 @@ class PrivateAppointmentTests(TestCase):
         res = self.client.post(APPOINTMENTS_URL, data=payload)
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
 
+    def test_create_basic_appointment_not_available(self):
+        """Test creating appointment scheduling not available"""
+
+        app_scheduling = AppointmentScheduling.objects.create(
+            pediatrician=self.pediatrician,
+            time_start=timezone.now() - datetime.timedelta(hours=1),
+            time_finish=timezone.now(),
+            is_available=False,
+        )
+        payload = {
+            "user": self.user.id,
+            "appointment_scheduling": app_scheduling.id,
+            "comments": "One comments",
+        }
+        res = self.client.post(APPOINTMENTS_URL, data=payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
     def test_retrieve_appointment_list(self):
         """Test retrieving list of appointment"""
         Appointment.objects.create(
